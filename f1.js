6f6c8b4f3ab663950cd15d256feebdb66a372bbb
raj-j-shah <rajcoder19@gmail.com>
2023-06-09 12:49:36 +0530

    first commit

---
+++ f1.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const {instrument} = require('@socket.io/admin-ui')
+const app = express();
+const {Server} = require('socket.io');
+const http = require('http');
+
+const io = new Server({
+    cors: {
+      origin: '*'
+    }
+  });
+  
+  io.listen(4000);
+  
+
+const port = process.env.port || 3003;
+
+io.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('groom',(prevr,value,cb) => {
+      socket.leave(prevr);
+      console.log('client left ' );
+      socket.join(value);
+      console.log('client joined '+value);
+      cb();
+    })
+   socket.on('msg',(value,rvalue) => {
+    if(rvalue==="")
+    socket.broadcast.emit('msg',value);
+    else
+    socket.to(rvalue).emit('msg',value);
+    console.log(value,socket.id);
+   })
+});
+
+instrument(io,{auth:false})
+
+
+
