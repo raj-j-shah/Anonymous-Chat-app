627bfea4df8b72f9c50b973c0a02f8b28d175e89
raj-j-shah <rajcoder19@gmail.com>
2023-06-09 14:55:20 +0530

    changes

--- f1.js
+++ f1.js
@@ -1,44 +1,40 @@
 const express = require('express');
-const {instrument} = require('@socket.io/admin-ui')
-const app = express();
-const {Server} = require('socket.io');
+const { instrument } = require('@socket.io/admin-ui');
+const { Server } = require('socket.io');
 const http = require('http');
 
-const io = new Server({
-    cors: {
-      origin: '*'
-    }
-  });
-  
- 
-  
-
-const port = process.env.PORT || 3003;
-io.listen(port);
-
-io.on("connect_error", (err) => {
-    console.log(`connect_error due to ${err.message}`);
-  });
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('groom',(prevr,value,cb) => {
-      socket.leave(prevr);
-      console.log('client left ' );
-      socket.join(value);
-      console.log('client joined '+value);
-      cb();
-    })
-   socket.on('msg',(value,rvalue) => {
-    if(rvalue==="")
-    socket.broadcast.emit('msg',value);
-    else
-    socket.to(rvalue).emit('msg',value);
-    console.log(value,socket.id);
-   })
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
 });
 
-instrument(io,{auth:false})
+const port = process.env.PORT || 4000;
 
+io.on('connect_error', (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('groom', (prevr, value, cb) => {
+    socket.leave(prevr);
+    console.log('client left');
+    socket.join(value);
+    console.log('client joined ' + value);
+    cb();
+  });
+  socket.on('msg', (value, rvalue) => {
+    if (rvalue === '') socket.broadcast.emit('msg', value);
+    else socket.to(rvalue).emit('msg', value);
+    console.log(value, socket.id);
+  });
+});
 
+instrument(io, { auth: false });
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
